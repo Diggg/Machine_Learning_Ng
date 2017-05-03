1d4f5e0f8b66acdc6d353764ce75141993b3e43b
Wang Zhen <wgzhn@163.com>
2017-05-03 21:48:20 +0800

    nn Cost

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -30,6 +30,7 @@ J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
 
+cal_type = 2; % 1 Matrix 2 For-Loop
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
 %               following parts.
@@ -39,23 +40,47 @@ Theta2_grad = zeros(size(Theta2));
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 %
+if (cal_type == 1)
+	Y = zeros(m,num_labels);
+	for i=1:m
+		%Y(i,rem(y(i),num_labels)+1)=1; 不用把10转成的0放在第一位
+		Y(i,y(i))=1;
+	end
+	%%Theta1: hidden_layer_size * (input_layer_size + 1)
+	%%Theta2: num_labels * (hidden_layer_size + 1)
 
-Y = zeros(m,num_labels);
-for i=1:m
-	Y(i,rem(y(i),num_labels)+1)=1;
-end
-%%Theta1: hidden_layer_size * (input_layer_size + 1)
-%%Theta2: num_labels * (hidden_layer_size + 1)
-
-a1 = [ones(m,1) X]; 		%m*(input_layer_size+1)
-z2 = a1*Theta1'; 		%m*hidden_layer_size
-a2 = [ones(m,1) sigmoid(z2)]; 	%m*(hidden_layer_size+1)
-z3 = a2*Theta2'; 		%m*num_labels
-h  = sigmoid(z3);		%m*num_labels
-J = (-Y.*log(h)-(1-Y).*log(1-h));
-%J = sum(sum((-Y.*log(h)-(1-Y).*log(1-h))));
-
-
+	% 使用矩阵进行计算  calculate by using matix
+	a1 = [ones(m,1) X]; 		%m*(input_layer_size+1)
+	z2 = a1*Theta1'; 		%m*hidden_layer_size
+	a2 = [ones(m,1) sigmoid(z2)]; 	%m*(hidden_layer_size+1)
+	z3 = a2*Theta2'; 		%m*num_labels
+	h  = sigmoid(z3);		%m*num_labels
+	%J = (-Y.*log(h)-(1-Y).*log(1-h));
+	J = 1/m*sum(sum((-Y.*log(h)-(1-Y).*log(1-h))));
+elseif (cal_type == 2)
+% calculate  using for-loop
+	for i=1:m
+		fprintf('Calculating X(%i)...\r',i)
+		a1=[1 X(i,:)];
+		z2 = a1*Theta1';
+		a2 = [1 sigmoid(z2)];
+		z3 = a2*Theta2';
+		h  = sigmoid(z3);
+		yy = zeros(1,num_labels);
+		yy(y(i)) = 1;
+		for k=1:num_labels
+			J = J + (-yy(k)*log(h(k))-(1-yy(k))*log(1-h(k)));
+		end
+		if exist('OCTAVE_VERSION')
+			fflush(stdout);
+		end
+	end
+	%Do not forget to devide J by m
+	J = J/m;
+endif
+%% Regularized cost function
+%%Note that you should not be regularizing the terms that correspond to the bias.
+J = J + lambda/(2*m)*(sum(sum(Theta1(:,2:end).*Theta1(:,2:end)))+sum(sum(Theta2(:,2:end).*Theta2(:,2:end))));
 
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
